413697fc0067cffabd7fd71eb0b7f6bc3b463872
Near Shelby <74830167+nearshelby-yt@users.noreply.github.com>
2021-03-12 18:11:02 -0300

    Debian and derivatives/Termux

---
+++ install.sh
@@ -0,0 +1,10 @@
+clear
+echo "Downloading essential things for operation\n"
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install python-pip -y
+apt install python3-pip -y
+pip3 install requests
+clear
+echo "Ready, now just type > python3 main.py"
